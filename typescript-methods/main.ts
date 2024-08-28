a5370c207ffe980c234b6187610ae9585465894a
Zoobia Syyed <syyedzoobia@gmail.com>
2024-08-28 21:28:25 +0000

    completed typescript methods

---
+++ typescript-methods/main.ts
@@ -0,0 +1,55 @@
+// math object
+const heroes = ['superman', 'batman', 'deadpool', 'ironman'];
+let randomNumber = Math.random();
+randomNumber = randomNumber * heroes.length;
+const randomIndex = Math.floor(randomNumber);
+console.log(randomIndex);
+const randomHero = heroes[randomIndex];
+console.log(randomHero);
+
+// array methods
+
+const library = [
+  { title: 'Harry Potter and the Prisoner of Azkaban', author: 'JK Rowling' },
+  { title: 'It Ends with Us', author: 'Coleen Hoover' },
+  { title: 'Percy Jackson', author: 'Rick Riordan' },
+];
+
+const lastBook = library.pop();
+console.log(lastBook);
+const firstBook = library.shift();
+console.log(firstBook);
+
+const js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer',
+};
+const css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou',
+};
+
+library.push(js);
+library.unshift(css);
+library.splice(1);
+console.log(library);
+
+// string methods
+const fullName = 'Zoobia Syyed';
+const firstAndLastName = fullName.split(' ');
+console.log(firstAndLastName);
+const firstName = firstAndLastName[0];
+const sayMyName = firstName.toUpperCase();
+console.log(sayMyName);
+
+// Object Methods
+const employee = {
+  name: 'Zoobia',
+  age: '23',
+  position: 'student',
+};
+
+const employeeKeys = Object.keys(employee);
+console.log(employeeKeys);
+const employeePairs = Object.entries(employee);
+console.log(employeePairs);
---
+++ typescript-methods/typescript-methods-quiz-notes.md
@@ -0,0 +1,61 @@
+# typescript-methods-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- Why do we log things to the console?
+
+- What is a method?
+
+- How is a method different from any other function?
+
+- How do you remove the last element from an array?
+
+- How do you round a number down to the nearest integer?
+
+- How do you generate a random number?
+
+- How do you delete an element from an array?
+
+- How do you append an element to an array?
+
+- How do you break a string up into an array?
+
+- Do string methods change the original string? How would you check if you weren't sure?
+
+- Is the return value of a function or method useful in every situation?
+
+- How do you get a collection of an objects keys?
+
+- How do you get a collection of an objects values?
+
+- How do you get a collection of both an objects keys and values?
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
