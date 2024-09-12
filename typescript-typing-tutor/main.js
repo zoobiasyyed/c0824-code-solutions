40421a529acf16442b1027c6954ca7931e6614ad
Zoobia Syyed <syyedzoobia@gmail.com>
2024-09-12 22:13:30 +0000

    completed typescript typing tutor

---
+++ typescript-typing-tutor/main.js
@@ -0,0 +1,15 @@
+'use strict';
+// querying the DOM for all span elements
+let x = 0;
+const $span = document.querySelectorAll('span');
+document.addEventListener('keydown', (event) => {
+  if ($span[x].textContent === event.key && x <= $span.length) {
+    console.log('yes');
+    $span[x].className = 'span span-green';
+  } else {
+    console.log('no');
+    $span[x].className = 'span span-red';
+  }
+  console.log(event);
+  x++;
+});
