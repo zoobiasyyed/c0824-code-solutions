2fb15f3b0ce2975d90d0ff1bfd9631b131463d40
Zoobia Syyed <syyedzoobia@gmail.com>
2024-09-13 00:45:34 +0000

    fixed border issue

--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -4,9 +4,10 @@ let x = 0;
 const $span = document.querySelectorAll('span');
 
 document.addEventListener('keydown', (event: KeyboardEvent) => {
-  if ($span[x].textContent === event.key && x <= $span.length) {
+  if ($span[x].textContent === event.key && x < $span.length - 1) {
     console.log('yes');
     $span[x].className = 'span span-green';
+    $span[x + 1].className = 'span border-selected';
     x++;
   } else {
     console.log('no');
