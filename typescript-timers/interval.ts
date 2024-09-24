b07c0f8a22b9ca0ad3623d4c2dbf5bca46e9999c
Zoobia Syyed <syyedzoobia@gmail.com>
2024-09-24 18:03:22 +0000

    completed typescript-timers

---
+++ typescript-timers/interval.ts
@@ -0,0 +1,13 @@
+let count = 4;
+const $countDownDisplay = document.querySelector('.countdown-display');
+if (!$countDownDisplay) throw new Error('$countDownDisplay query failed');
+
+const intervalId = setInterval(function () {
+  if (count > 0) {
+    $countDownDisplay.textContent = count.toString();
+    count--;
+  } else {
+    $countDownDisplay.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(intervalId);
+  }
+}, 1000);
