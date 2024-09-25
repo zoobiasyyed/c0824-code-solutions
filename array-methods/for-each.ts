042afb5d050744d609389e404a8cfe65a5a0a9d2
Zoobia Syyed <syyedzoobia@gmail.com>
2024-09-25 22:44:00 +0000

    completed array-methods

---
+++ array-methods/for-each.ts
@@ -0,0 +1,6 @@
+const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
+
+values.forEach((element) => console.log('in order:', element));
+values.forEach((reverseElement, index) => {
+  console.log('reverse: ', values[values.length - 1 - index]);
+});
