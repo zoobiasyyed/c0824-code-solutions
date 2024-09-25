9f21f121f7205a0b859830b44d2353be4829068e
Zoobia Syyed <syyedzoobia@gmail.com>
2024-09-25 16:45:50 +0000

    completed typescript-destructuring

---
+++ typescript-destructuring/main.ts
@@ -0,0 +1,91 @@
+// creating an interface
+
+interface Book {
+  title: string;
+  author: string;
+  libraryId: number;
+}
+
+const book1: Book = {
+  title: 'Goodnight Punpun',
+  author: 'Inio Asano',
+  libraryId: 3353,
+};
+
+// const { firstName, lastName } = person;
+
+const { title, author, libraryId } = book1;
+
+console.log(
+  'the title of the book is' +
+    `${title}` +
+    ', the author is ' +
+    `${author}` +
+    ', and the library id is ' +
+    `${libraryId}`
+);
+
+const book2: Book = {
+  title: 'Les Fleurs du mal',
+  author: 'Charles Baudelaire',
+  libraryId: 2345,
+};
+
+const { title: title2, author: author2, libraryId: libraryId2 } = book2;
+
+console.log(
+  'the title of the book is' +
+    `${title2}` +
+    ', the author is ' +
+    `${author2}` +
+    ', and the library id is ' +
+    `${libraryId2}`
+);
+
+// creating array of objects
+const library: Book[] = [
+  {
+    title: 'The Road Ahead',
+    author: 'Bill Gates',
+    libraryId: 1254,
+  },
+  {
+    title: 'Walter Isaacson',
+    author: 'Steve Jobs',
+    libraryId: 4264,
+  },
+  {
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    author: 'Suzanne Collins',
+    libraryId: 3245,
+  },
+  {
+    title: "Dead Dead Demon's De De De De Destruction",
+    author: 'Inio Asano',
+    libraryId: 1233,
+  },
+];
+
+const [book3, book4, book5] = library;
+
+// const { firstName, lastName } = person;
+
+const { title: title3, author: author3, libraryId: libraryId3 } = book3;
+
+console.log(book3);
+
+const { title: title4, author: author4, libraryId: libraryId4 } = book4;
+
+console.log(book4);
+
+const { title: title5, author: author5, libraryId: libraryId5 } = book5;
+
+console.log(book5);
+
+// destructure last object
+
+const [, , , book6] = library;
+
+const { title: title6, author: author6, libraryId: libraryId6 } = book6;
+
+console.log(book6);
