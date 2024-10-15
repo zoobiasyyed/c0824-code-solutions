218d554fd23cddc281afddb6b24146b2472ecaf3
Zoobia Syyed <syyedzoobia@gmail.com>
2024-10-15 16:37:27 +0000

    completed react-components

---
+++ react-components/my-app/src/ImageCaption.tsx
@@ -0,0 +1,3 @@
+export function ImageCaption() {
+  return <h3>This is a Pokemon Image</h3>;
+}
---
+++ react-components/my-app/src/ImageDescription.tsx
@@ -0,0 +1,18 @@
+export function ImageDescription() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis,
+        ex ac vestibulum consequat, turpis eros mattis mi, eget elementum augue
+        tortor vel magna. Suspendisse potenti. Mauris luctus magna eget pulvinar
+        mattis. Curabitur accumsan tellus et sapien aliquet, a imperdiet ligula
+        molestie. Duis sed tellus dolor. Proin vestibulum vehicula laoreet.
+        Phasellus accumsan, dui at ornare luctus, magna odio varius dolor, ac
+        interdum mauris ipsum sed nibh. Morbi sit amet nisi id ipsum rhoncus
+        malesuada et efficitur turpis. Nunc hendrerit a sem vitae mollis.
+        Aliquam sit amet ipsum odio. Nunc vitae eros sit amet massa malesuada
+        viverra.
+      </p>
+    </>
+  );
+}
