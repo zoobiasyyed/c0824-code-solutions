d47f3c840a3cb070e22885e5d039ba49a219b4ba
Zoobia Syyed <syyedzoobia@gmail.com>
2024-10-23 04:52:59 +0000

    added some comments

--- react-multiple-pages/my-app/src/App.tsx
+++ react-multiple-pages/my-app/src/App.tsx
@@ -9,9 +9,20 @@ export function App() {
   return (
     <Router>
       <Routes>
+        {/* This path '/' is the root path of the application.
+        element will define the component for this root */}
+        {/* the LINK tags will basically be like ur anchor or
+        href elements */}
         <Route path="/" element={<Header />}>
+          {' '}
           <Route index element={<Dashboard />} />
           <Route path="details/:itemId" element={<Details />} />
+          {/* the /: defines a custom parameter which is dynamic and
+          able to be changed. anything that comes after the slash
+          will be stored in the itemId
+
+          useParams will return a object that will have all the custom
+          parameters we defined in our route  */}
           <Route path="about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>
