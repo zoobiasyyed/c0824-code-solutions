18e300189088da7a5ffc3acce4270bcf9c1ee87b
Zoobia Syyed <syyedzoobia@gmail.com>
2024-10-24 05:35:37 +0000

    completed react-app-drawer

--- react-app-drawer/my-app/src/components/Header.tsx
+++ react-app-drawer/my-app/src/components/Header.tsx
@@ -1,8 +1,18 @@
 import { Link, Outlet } from 'react-router-dom';
-import { FaBars, FaBug, FaBook } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
 import { useState } from 'react';
 
-export function Header() {
+export type MenuItem = {
+  name: string;
+  iconUrl: string;
+  path: string;
+};
+
+type Props = {
+  menuItems: MenuItem[];
+};
+
+export function Header({ menuItems }: Props) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOpen = () => {
@@ -13,25 +23,26 @@ export function Header() {
     <div className="flex flex-row w-full">
       <nav
         className={`flex flex-col py-2 px-3 bg-gray-900 transition-width duration-300 ease-in-out ${
-          isOpen ? 'w-40' : 'w-[40px]'
+          isOpen ? 'w-40' : 'w-[50px]'
         }`}>
         <FaBars onClick={handleOpen}></FaBars>
-        <div className="mt-5 text-white flex items-center ">
-          <FaBug />
-          {isOpen && (
-            <Link className="text-white ml-2 " to="about">
-              About
-            </Link>
-          )}
-        </div>
-        <div className="mt-5 text-white flex items-center">
-          <FaBook />
-          {isOpen && (
-            <Link className="text-white ml-2" to="/">
-              Catalog
-            </Link>
-          )}
-        </div>
+        {isOpen && <h3 className="mt-3">Hylian Shopping</h3>}
+        {menuItems.map((menu) => (
+          <div className="flex items-center mt-5" key={menu.name}>
+            <img
+              className="w-[30px] text-white"
+              src={menu.iconUrl}
+              alt={menu.name}
+              style={{ filter: 'brightness(0) invert(1)' }}
+            />
+
+            {isOpen && (
+              <Link to={menu.path} className="ml-2 text-white">
+                {menu.name}
+              </Link>
+            )}
+          </div>
+        ))}
       </nav>
       <div className="grow">
         <Outlet />
