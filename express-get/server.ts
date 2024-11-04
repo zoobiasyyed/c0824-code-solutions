1fcd3a393294ae72641851f5dad26a63e91478bb
Zoobia Syyed <syyedzoobia@gmail.com>
2024-11-04 17:44:44 +0000

    completed express-get

---
+++ express-get/server.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import pg from 'pg';
+import { ClientError, errorMiddleware } from './lib/index.js';
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/pagila',
+  ssl: {
+    // Allow non-SSL traffic to localhost
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+
+app.get('/api/films', async (req, res, next) => {
+  try {
+    const sql = `
+    select "title",
+    "replacementCost"
+    from "films"
+    order by "replacementCost" DESC
+    limit 2;`;
+    const result = await db.query(sql);
+    const films = result.rows;
+    res.json(films);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/films/:filmId', async (req, res, next) => {
+  try {
+    const { filmId } = req.params;
+    if (filmId === undefined) {
+      throw new ClientError(400, 'filmId is required');
+    }
+    const sql = `
+    select "title",
+    "replacementCost", "filmId"
+    from "films"
+    WHERE "filmId" = $1`;
+    const params = [filmId];
+    const result = await db.query(sql, params);
+    const films = result.rows[0];
+    res.json(films);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
+
+app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
